016b6782dcb799d7ea231aabc6439384caf5e418
Soumyajit Kundu <52736752+soumyajitkundu@users.noreply.github.com>
2021-03-29 10:28:42 +0530

    Initial Commit

---
+++ Web Quiz/js/questions.js
@@ -0,0 +1,171 @@
+let questions = [
+    {
+    numb: 1,
+    question: "When was TCS founded?",
+    answer: "1968",
+    options: [
+      "1958",
+      "1968",
+      "1898",
+      "1972"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is the full form of AWS?",
+    answer: "Amazon Web Services",
+    options: [
+      "Assurance Web Systems",
+      "Akash Web Startup",
+      "Amazon Web Services",
+      "Awesome Worldwide Systems"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Who founded Unix?",
+    answer: "Ken Thompson",
+    options: [
+      "Dennis Ritchie",
+      "Ken Thompson",
+      "Steve Jobs",
+      "Harry McCullum"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+
+    {
+     numb: 6,
+     question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+
+    {
+     numb: 7,
+     question: "What is the full form of SDLC?",
+    answer: "Software Development Life Cycle",
+    options: [
+      "Source Deploy Longivity Circuit",
+      "Software Development Life Cycle",
+      "Secure Destroy Longterm Cycle",
+      "Super Delite Lengthy Cycle"
+    ]
+  },
+
+    {
+     numb: 8,
+     question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+     numb: 9,
+     question: "What does GCP stand for?",
+    answer: "Google Cloud Platform",
+    options: [
+      "Google Cyber Product",
+      "Google Central Platform",
+      "Google Cloud Platform",
+      "Google Cloud Product"
+    ]
+  },
+    {
+     numb: 10,
+     question: "What is the capital of India?",
+    answer: "New Delhi",
+    options: [
+      "New Delhi",
+      "Lucknow",
+      "Kolkata",
+      "Allahabad"
+    ]
+  },
+    {
+     numb: 11,
+     question: "What is an SSD?",
+    answer: "Solid State Drive",
+    options: [
+      "Solid State Device",
+      "Solid Structure Device",
+      "Solid State Drive",
+      "Solid Solar Drive"
+    ]
+  },
+    {
+     numb: 12,
+     question: "Full form of PHP?",
+    answer: "HyperText Preprocessor",
+    options: [
+      "HyperText Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+     numb: 13,
+     question: "What does CLI stand for?",
+    answer: "Command Line Interface",
+    options: [
+      "Cloud Line Interface",
+      "Command Line Interface",
+      "Command Line Iterate",
+      "Cloud Line Iterate"
+    ]
+  },
+    {
+     numb: 14,
+     question: "What does S3 stand for?",
+    answer: "Simple Storage Service",
+    options: [
+      "Simple Storage Statement",
+      "Super Storage Service",
+      "Simple Storage Service",
+      "Simple Storage Solution"
+    ]
+  },
+    {
+     numb: 15,
+     question: "What does EC2 stand for?",
+    answer: "Elastic Compute Cloud",
+    options: [
+      "Elastic Command Compute",
+      "Elastic Command Cloud",
+      "Elastic Cloud Compute",
+      "Elastic Compute Cloud"
+    ]
+  },
+
+];
